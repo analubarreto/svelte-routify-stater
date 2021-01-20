0d0d841a0a2dd927e18297fe746e685baf90319a
analubarreto <analuibm@gmail.com>
2021-01-19 16:56:54 -0800

    changed pages styles

--- src/pages/about.svelte
+++ src/pages/about.svelte
@@ -1,1 +1,27 @@
-<h1 class="text-4xl mb-5 mt-5">About Page</h1>
+<script lang="ts">
+	const features: string[] = ['Prettier', 'ESLint', 'Typescript', 'Tailwind', 'PostCSS'];
+</script>
+
+<main class="flex flex-col mx-8 my-10">
+	<h1 class="text-4xl mb-5 mt-5">About Page</h1>
+	<p>
+		This is a Svelte Routify starter template slightly changed from the <a
+			href="https://github.com/roxiness/routify-starter"
+			target="_blank">original Routify starter template</a
+		>.
+	</p>
+	<p>By default, this adds:</p>
+	<ul class="self-start">
+		{#each features as feature}
+			<li>- {feature}</li>
+		{/each}
+	</ul>
+	<p>Along with everything else routify adds.</p>
+	<p>This also has a blog starter logic.</p>
+	<p>
+		If you have any issues and or sugestions, please report at <a
+			href="https://github.com/analubarreto/svelte-starter"
+			target="_blank">the github repo</a
+		>.
+	</p>
+</main>
--- src/pages/blog/index.svelte
+++ src/pages/blog/index.svelte
@@ -3,9 +3,11 @@
 	import posts from '../../components/blog/blog-store';
 </script>
 
-<h1 class="text-4xl mb-5 mt-5">Blog Page</h1>
-<ul>
-	{#each $posts as post}
-		<li><a href={$url(`./${post._id}`)}>{post.title}</a></li>
-	{/each}
-</ul>
+<main class="flex flex-col mx-8 my-10">
+	<h1 class="text-4xl mb-5 mt-5">Blog Page</h1>
+	<ul>
+		{#each $posts as post}
+			<li><a href={$url(`./${post._id}`)}>{post.title}</a></li>
+		{/each}
+	</ul>
+</main>
